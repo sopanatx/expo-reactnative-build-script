a563fdf9799a4e8675b0e6011ff0d72cc62a18b2
WasuthornX <sopanus2014@gmail.com>
2020-09-15 08:04:02 +0700

    Create build-android.sh

---
+++ build-android.sh
@@ -0,0 +1,7 @@
+EXPO_ANDROID_KEYSTORE_PASSWORD="" \
+EXPO_ANDROID_KEY_PASSWORD="" \
+turtle build:android \
+  --keystore-path path-to-file.jks \
+  --keystore-alias xxxxxxx \
+  --username xxxxx \
+  --password xxxxxx
